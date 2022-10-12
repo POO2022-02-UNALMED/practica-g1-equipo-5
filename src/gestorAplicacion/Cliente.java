926c176449eb03b11468574c81b8cd513a0231fb
Soandra <soandradep@unal.edu.co>
2022-10-12 13:49:54 -0500

    Clases desde usuario correcto

--- src/gestorAplicacion/Cliente.java
+++ src/gestorAplicacion/Cliente.java
@@ -1,5 +1,37 @@
 package gestorAplicacion;
 
 public class Cliente {
+    private String nombre;
+    private Cuenta cuenta;
+    private int cedula;
 
+    public Cliente(String nombre, Cuenta cuenta, int cedula) {
+        this.nombre = nombre;
+        this.cuenta = cuenta;
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Cuenta getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
 }
--- src/gestorAplicacion/Cuenta.java
+++ src/gestorAplicacion/Cuenta.java
@@ -1,5 +1,70 @@
 package gestorAplicacion;
 
 public class Cuenta {
+    private int numero;
+    private String tipo;
+    private int saldo;
+    private Prestamo deuda;
+    private boolean estado;
+    private Tarjeta tarjeta;
 
+    public Cuenta(int numero, String tipo, Prestamo deuda, boolean estado, Tarjeta tarjeta) {
+        this.numero = numero;
+        this.tipo = tipo;
+        this.deuda = deuda;
+        this.estado = estado;
+        this.tarjeta = tarjeta;
+    }
+
+    public void aumentarSaldo(int cantidad){
+        if (estado){
+            saldo += cantidad;
+        }
+    }
+
+    public void disminuirSaldo(int cantidad){
+        if (estado && cantidad < saldo){
+            saldo -= cantidad;
+        }
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Prestamo getDeuda() {
+        return deuda;
+    }
+
+    public void setDeuda(Prestamo deuda) {
+        this.deuda = deuda;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public Tarjeta getTarjeta() {
+        return tarjeta;
+    }
+
+    public void setTarjeta(Tarjeta tarjeta) {
+        this.tarjeta = tarjeta;
+    }
 }
