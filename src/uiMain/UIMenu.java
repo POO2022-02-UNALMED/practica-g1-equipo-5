5350cedfdda11ce81763edfedb4a500912a30e36
Soandra <soandradep@unal.edu.co>
2022-10-23 19:47:47 -0500

    UI de solicitar prestamo

--- src/uiMain/UIMenu.java
+++ src/uiMain/UIMenu.java
@@ -29,12 +29,7 @@ public class UIMenu {
 
             switch (opcion){
                 case 1:
-                    System.out.println("Puedes solicitar un prestamo entre 500.000 y 7'000.000 a 24 cuotas\n Ingresa el valor a solicitar");
-                    int valor=sc.nextInt();
-                    System.out.println("Elige el tipo de prestamo (universitario,hobbie,libre)");
-                    String tipoPrestamo =sc.next();
-                    ((CuentaAhorro)cliente.cuenta).solicitarPrestamo(valor,tipoPrestamo);
-                    System.out.println(((CuentaAhorro) cliente.cuenta).getPrestamo().toString());
+                    UISolicitarPrestamo.solicitarPrestamo(cliente);
                     break;
                 case 2:
 
