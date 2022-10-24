09566eb9551639b1cd8c7b083d3cfb22ac5ee9a0
Soandra <soandradep@unal.edu.co>
2022-10-23 20:37:05 -0500

    Deserializador incompleto

---
+++ src/baseDatos/Deserializador.java
@@ -0,0 +1,79 @@
+package baseDatos;
+
+import gestorAplicacion.transacciones.Bolsillo;
+import gestorAplicacion.transacciones.Prestamo;
+import gestorAplicacion.usuario.Cliente;
+import gestorAplicacion.usuario.CuentaAhorro;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deserializador {
+    private static File rutaTemp = new File("src\\baseDatos\\temp");
+
+    public static void deserializar(Cliente cliente){
+        File[] docs = rutaTemp.listFiles();
+        FileInputStream fis;
+        ObjectInputStream ois;
+
+        for (File file : docs) {
+            if (file.getAbsolutePath().contains("prestamos.txt")){
+                try {
+                    fis = new FileInputStream(file);
+                    ois = new ObjectInputStream(fis);
+
+                    ((CuentaAhorro) cliente.cuenta).setPrestamo(Prestamo ois.readObject());
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
+                } catch (IOException e){
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e){
+                    e.printStackTrace();
+                }
+            } else if (file.getAbsolutePath().contains("bolsillos.txt")) {
+                try {
+                    fis = new FileInputStream(file);
+                    ois = new ObjectInputStream(fis);
+
+                    cliente.getCuenta().setMisBolsillos((ArrayList<Bolsillo>) ois.readObject());
+                } catch (FileNotFoundException e){
+                    e.printStackTrace();
+                } catch (IOException e){
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e){
+                    e.printStackTrace();
+                }
+
+                //Corregir desde aquí
+            } else if (file.getAbsolutePath().contains("pagos.txt")) {
+                try {
+                    fis = new FileInputStream(file);
+                    ois = new ObjectInputStream(fis);
+
+                    cliente.getCuenta().setMisBolsillos((ArrayList<Bolsillo>) ois.readObject());
+                } catch (FileNotFoundException e){
+                    e.printStackTrace();
+                } catch (IOException e){
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e){
+                    e.printStackTrace();
+                }
+            } else if (file.getAbsolutePath().contains("trasnferencias.txt")) {
+                try {
+                    fis = new FileInputStream(file);
+                    ois = new ObjectInputStream(fis);
+
+                    cliente.getCuenta().setMisBolsillos((ArrayList<Bolsillo>) ois.readObject());
+                } catch (FileNotFoundException e){
+                    e.printStackTrace();
+                } catch (IOException e){
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
